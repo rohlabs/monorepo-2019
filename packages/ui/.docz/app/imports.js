65d6aa0839455ac56babeb38eef46f6a979cd3cf
MH Rohman Masyhar <mhrohman@live.com>
2019-01-10 10:15:53 +0700

    fix untracted files

--- packages/ui/.docz/app/imports.js
+++
@@ -1,4 +0,0 @@
-export const imports = {
-  'src/components/Button/index.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-button-index" */ 'src/components/Button/index.mdx'),
-}
